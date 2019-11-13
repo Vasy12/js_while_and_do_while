4deea08bd107b9100a81a48dae5384e56f8d7412
User Name <artur.it2914@gmail.com>
2019-11-13 17:04:44 +0200

    Create while do_while tasks

---
+++ assets/js/index.js
@@ -0,0 +1,117 @@
+"use strict";
+
+
+let stay = true;
+
+do {
+
+    const taskNumber = prompt("Which task do you want (from 1 to 10, press \"E\" to exit)?")
+
+    switch (taskNumber) {
+        case "1": {
+
+            const userValue = prompt("Please input your count");
+
+            let i = 0;
+            while (i++ < userValue) {
+
+                console.log("#");
+
+            }
+
+
+        }
+            break;
+        case "2": {
+            const userValue = prompt("Please input your count");
+
+            let userNumber = Number(userValue);
+
+            if (userNumber >= 0) {
+                while (userNumber >= 0) {
+
+                    console.log(userNumber--);
+
+                }
+            } else {
+                while (userNumber <= 0) {
+
+                    console.log(userNumber++);
+
+                }
+            }
+
+
+        }
+            break;
+        case "3": {
+            const userValue = +prompt("Please input your number");
+            const userExp = prompt("Please input your exponent");
+
+            let result = userValue;
+            let i = 1;
+
+            while (i++ < userExp) {
+
+                result *= userValue;
+
+
+            }
+
+            console.log(result);
+
+
+        }
+            break;
+        case "4": {
+            console.log(taskNumber);
+        }
+            break;
+        case "5": {
+            console.log(taskNumber);
+        }
+            break;
+        case "6": {
+            console.log(taskNumber);
+        }
+            break;
+        case "7": {
+            console.log(taskNumber);
+        }
+            break;
+        case "8": {
+            console.log(taskNumber);
+        }
+            break;
+        case "9": {
+            console.log(taskNumber);
+        }
+            break;
+        case "10": {
+            console.log(taskNumber);
+        }
+            break;
+        case "E":
+        case "e": {
+            console.log(taskNumber);
+
+            stay = false;
+        }
+            break;
+
+
+    }
+
+
+} while (stay);
+
+
+console.log("EXIT!");
+
+
+
+
+
+
+
+
